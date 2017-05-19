d8bfa843506a207280a0dd6f7b81d7fc9fab21cf
yurik256 <yurik256@gmail.com>
2017-05-19 16:05:18 +0200

    add simple combineReducers implementation

--- src/redux-implementation/simple-redux.js
+++ src/redux-implementation/simple-redux.js
@@ -20,3 +20,12 @@ const createStore = reducer => {
 
   return { getState, dispatch, subscribe };
 };
+
+const combineReducers = reducers => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce((nextState, key) => {
+      nextState[key] = reducers[key](state[key], action);
+      return nextState;
+    }, {});
+  };
+};
